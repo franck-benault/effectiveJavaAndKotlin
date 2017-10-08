ec78ef2bfae14c29806f3f7545199df5632ae965
franck-benault <franck-benault@orange.fr>
2017-10-08 23:28:31 +0200

    item2 builder

---
+++ Item02Builder/src/main/java/hello/Application.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Application {
+
+	public static void main(String[] args) {
+		NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).
+				  calories(100).sodium(35).carbohydrate(27).build();
+
+	}
+
+}
