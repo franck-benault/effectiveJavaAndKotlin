a1be61474b5e52789f868e7d81f42c3500382ed7
franck-benault <franck-benault@orange.fr>
2017-10-16 22:35:52 +0200

    add utility class example in java

---
+++ item04UtilityClassesPrivateConstructor/src/main/java/hello/Arrays.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Arrays {
+	
+	
+	//ensuring non-instantiability
+	private Arrays() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	public static void sort(int[] tab) {
+		//TODO ...
+	}
+
+}
---
+++ item04UtilityClassesPrivateConstructor/src/test/java/hello/ArraysTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+
+import org.junit.Test;
+
+public class ArraysTest {
+
+	@Test
+	public void test() {
+		int tabInt[] = {0,1,2,3,4,5,6,7,8,9};
+		Arrays.sort(tabInt);
+	}
+
+}
