1539ccdacd5b827c461eddf8fb97f312ebeb9ade
franck-benault <franck-benault@orange.fr>
2017-10-17 18:36:41 +0200

    add some comment in kotlin example

--- item05AvoidCreatingUnnecessaryObjectskt/src/main/kotlin/hello/main.kt
+++ item05AvoidCreatingUnnecessaryObjectskt/src/main/kotlin/hello/main.kt
@@ -1,11 +1,19 @@
 package hello
 
-fun main(args : Array<String>) { 
+fun main(args : Array<String>) {
+	
+	//bad
+	//var sum: Long? = 0L
+	
+	//good
+	// var sum: Long = 0L 
 	var sum = 0L;
 		
 	val startTime = System.currentTimeMillis();
 	for (i in  0..Integer.MAX_VALUE) {
-			sum += i
+			//bad
+			//sum = sum?.plus(i)
+		sum +=i
 	}
 	val endTime = System.currentTimeMillis()
 	System.out.println(endTime-startTime)
